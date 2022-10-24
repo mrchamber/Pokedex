32a71f50c77cc70c4d6d9837e9ad05a432a33edf
mrchamber <60364670+mrchamber@users.noreply.github.com>
2022-10-24 00:18:11 -0400

    got rid of "'"

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const displayPokemon = (pokemon) => {
         <h2>${pokeman.id}. ${pokeman.name}</h2>
         <p>Type: ${pokeman.type}</p>
     </li>
-    `)
+    `).join('');
     pokedex.innerHTML = pokemonHTMLString;
 }
 
