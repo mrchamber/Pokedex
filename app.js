357004431c0fc0196da215bc79c06c60d0f40326
mrchamber <60364670+mrchamber@users.noreply.github.com>
2022-10-27 02:59:08 -0400

    Updated look

--- app.js
+++ app.js
@@ -1,96 +1,49 @@
-const pokedex = document.getElementById("pokedex");
+const poke_container = document.getElementById("poke-container");
+const pokemon_count = 151;
+const colors = {
+    generation1: "linear-gradient(180deg, " +'#1111ff' + ", " + '#ff1111' + ", " + '#ffd733' +")",
+}
 
-console.log(pokedex);
+console.log(poke_container);
 
-const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 151; i++){
-        const url = `https://updated-pokemon-apis-production.up.railway.app/pokemon/${i}`;
-        promises.push(fetch(url).then((res)=> res.json()));
+const fetchPokemon =  async () => {
+    for (let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i);
     }
-
-    Promise.all(promises).then(results => {
-        const pokemon = results.map((data) => ({
-            name: data.name,
-            id: data.id,
-            image: data.image,
-            type: data.type,
-            abilities: data.abilities,
-            HP: data.stats.HP,
-            Attack: data.stats.Attack,
-            Defense: data.stats.Defense,
-            SpAtk: data.stats.SpAtk,
-            SpDef: data.stats.SpDef,
-            Speed: data.stats.Speed
-        }));
-        displayPokemon(pokemon);
-    });
-};
-
-const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-    const pokemonHTMLString = pokemon.map (pokeman => `
-    <div class="card" onclick="selectPokemon(${pokeman.id})">
-        <img class="card-img" src="${pokeman.image}"/>
-        <h2 class="card-num">#${pokeman.id.toString().padStart(3,'0')}</h2>
-        <h2 class="card-title">${pokeman.name}</h2>
-        <p class="card-subtitle">Type: ${pokeman.type}</p>
-    </div>
-    `).join('');
-    pokedex.innerHTML = pokemonHTMLString;
-};
-
-const selectPokemon = async (id) => {
+}
+const getPokemon = async (id) =>{
     const url = `https://updated-pokemon-apis-production.up.railway.app/pokemon/${id}`;
     const res = await fetch(url)
-    const pokeman = await res.json();
-    displayPopup(pokeman);
-};
-
-const displayPopup = (pokeman)  => {
-    const htmlString = `
-        <div class="popup">
-            <button id ="closeBtn" onclick="closePopup()">Close</button>
-            <div class="cardPop">
-            <img class="card-imgPop" src="${pokeman.image}"/>
-            <h2 class="card-num">#${pokeman.id.toString().padStart(3,'0')}</h2>
-            <h2 class="card-titlePop">${pokeman.name}</h2>
-            <p class="info"><small>Type: </small>${pokeman.type} | <small>Abilites: </small>${pokeman.abilities} | <small>Height: </small>${pokeman.height} | <small>Weight: </small>${pokeman.weight}</p>
-            <table class="info">
-                <tr>
-                    <th>Base Stats</th>
-                    <th>Base Stats</th>
-                </tr>
-                <tr>
-                    <td>HP</td>
-                    <td>${pokeman.stats.HP}</td>
-                </tr>
-                    <td>Attack</td>
-                    <td>${pokeman.stats.Attack}</td>
-                <tr>
-                    <td>Defense</td>
-                     <td>${pokeman.stats.Defense}</td>
-                </tr>
-                    <td>Sp. Attack</td>
-                    <td>${pokeman.stats.SpAtk}</td>
-                <tr>
-                    <td>Sp. Defense</td>
-                    <td>${pokeman.stats.SpDef}</td>
-                </tr>
-                <tr>
-                    <td>Speed</td>  
-                    <td>${pokeman.stats.Speed}</td>   
-                </tr>
-            </table>
-            </div>
-        </div>`;
-    pokedex.innerHTML = htmlString + pokedex.innerHTML;
-    console.log(htmlString);
-};
-
-const closePopup = () => {
-    const popup = document.querySelector('.popup');
-    popup.parentElement.removeChild(popup);
+    const data = await res.json()
+    createPokemonCard(data);
 }
 
 fetchPokemon();
+
+const  createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3,'0')
+    const generation = pokemon.generation.name
+    const color = colors[generation]
+
+    pokemonEl.style.background = color;
+
+
+    const pokemonHTMLString = `
+     <div class="img-container">
+                <img src="https://pokeimage-production.up.railway.app/pokeImg/${pokemon.id}.png">
+            </div>
+            <div class="info">
+                <span class="number">#${id}</span>
+                <h3 class="name">${name}</h3>
+                <small class="type">Type: <span>${pokemon.type}</span></small>
+            </div>
+            `
+    pokemonEl.innerHTML = pokemonHTMLString
+
+    poke_container.appendChild(pokemonEl)
+};
+
