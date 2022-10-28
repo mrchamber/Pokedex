2de02c1b0f1573e70d158533005eb2c2d5a532f8
mrchamber <60364670+mrchamber@users.noreply.github.com>
2022-10-27 21:08:46 -0400

    OnCLick window added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById("poke-container");
-const pokemon_count = 250;
+const pokemon_count = 201;
 const colors = {
     generation1: "linear-gradient(145deg, " +'#1111ff' + ", " + '#ff1111' + ", " + '#ffd733' +")",
     generation2: "linear-gradient(145deg, " +'#daa520' + ", " + '#c0c0c0' + ", " + '#4fd9ff' +")",
@@ -18,8 +18,6 @@ const getPokemon = async (id) =>{
     createPokemonCard(data);
 }
 
-fetchPokemon();
-
 const  createPokemonCard = (pokemon) => {
     const pokemonEl = document.createElement('div');
     pokemonEl.classList.add('pokemon')
@@ -37,12 +35,76 @@ const  createPokemonCard = (pokemon) => {
                 <img src="https://pokeimage-production.up.railway.app/pokeImg/${pokemon.id}.png">
             </div>
             <div class="info">
-                <span class="number">#${id}</span>
+                <span class="number" onclick="selectPokemon(${pokemon.id})">#${id}</span>
                 <h3 class="name">${name}</h3>
             </div>
             `
-    pokemonEl.innerHTML = pokemonHTMLString
+    pokemonEl.innerHTML = pokemonHTMLString;
 
     poke_container.appendChild(pokemonEl)
 };
+
+const selectPokemon = async (id) => {
+    const url = `https://updated-pokemon-apis-production.up.railway.app/pokemon/${id}`;
+    const res = await fetch(url);
+    const pokeman = await res.json();
+    displayCard(pokeman);
+};
+
+const displayCard = (pokeman) => {
+    const htmlString = `
+        <div class="poke-card">
+            <div class="poke-card-body">
+                <div class="left-side">
+                    <h1 class="num">#${pokeman.id.toString().padStart(3,'0')}</h1>
+                    <h2 class="card-name">${pokeman.name}</h2>
+                    <img src="https://pokeimage-production.up.railway.app/pokeImg/${pokeman.id}.png" class="card-img">
+                    <h3 class="poke-height">${pokeman.height}</h3>
+                    <h3 class="poke-height">${pokeman.weight}</h3>
+                </div>
+                <div class="right-side">
+                    <h2 class="section-header">Enrty:</h2>
+                    <div class="enrty-container">
+                        <p class="enrty-info"></p>
+                    </div>
+                    <h2 class="section-header">Abilities:</h2>
+                    <div class="ability-container">
+                         <p class="ability-info"><${pokeman.abilities}/p>
+                    </div>
+                    <h2 class="section-header">Base Stats</h2>
+                    <div class="bs-container">
+                        <div class="stat-columns">
+                            <div class="stat-name">HP</div>
+                            <divclass="stat-val">${pokeman.stats.HP}</div>
+                        </div>
+                        <div class="stat-columns">
+                            <div class="stat-name">Attack</div>
+                            <div class="stat-val">${pokeman.stats.Attack}</div>
+                        </div>
+                        <div class="stat-columns">
+                            <div class="stat-name">Defense</div>
+                            <div class="stat-val">${pokeman.stats.Defense}</div>
+                        </div>
+                        <div class="stat-columns">
+                            <div class="stat-name">Special Attack</div>
+                            <div class="stat-val">${pokeman.stats.SpAtk}</div>
+                        </div>
+                        <div class="stat-columns">
+                            <div class="stat-name">Special Defense</div>
+                            <div class="stat-val">${pokeman.stats.SpDef}</div>
+                        </div>
+                        <div class="stat-columns">
+                            <div class="stat-name">Speed</div>
+                            <div class="stat-val">${pokeman.stats.Speed}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    poke_container.innerHTML = htmlString + poke_container.innerHTML;
+    console.log(htmlString);
+}
+
+fetchPokemon();
 
