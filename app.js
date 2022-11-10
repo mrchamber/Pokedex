3e44a48e66a0aa597e468e31f678517f63d9c50a
mrchamber <60364670+mrchamber@users.noreply.github.com>
2022-11-10 13:16:12 -0500

    Hisui scheme change

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const colors = {
     generation6: "linear-gradient(145deg, " +'#6376b8' + ", " + '#ed5540' + ")",
     generation7: "linear-gradient(145deg, " +'#f1912b' + ", " + '#5599ca' + ")",
     generation8: "linear-gradient(145deg, " +'#00d1f6' + ", " + '#9e2306' + ")",
+    generation85: "linear-gradient(145deg, " +'#fad709' + ", " + '#f1f4f4' + ", " + '#5e6365' +")",
     generation9: "linear-gradient(145deg, " +'#c91421' + ", " + '#632ea6' + ")",
 }
 
